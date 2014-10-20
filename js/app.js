41f67bf1b064fe7af3fc54540df1f872c0a5b730
ReiterMichael <reiter-1997@gmx.at>
2014-10-20 21:48:19 +0200

    Add, Search
    Probleme beim Darstellen der Produktinformationen

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var app = angular.module('DerBewerter',['ngRoute', 'ngSanitize']);
 
+
+
 app.config(function($routeProvider){
 	$routeProvider
 		 .when("/welcomePage",{
@@ -14,9 +16,13 @@ app.config(function($routeProvider){
 		    templateUrl:"subCategory.html",
 		    controller:"MainController"
 		 })
-		 .when("/product",{
+		.when("/product",{
 			templateUrl:"product.html",
 			controller:"ProductController"
+		})
+		.when("/addProduct",{
+			templateUrl:"addProduct.html",
+			controller:"AddController"
 		})
 		.otherwise({
 			redirectTo:"welcomePage"
@@ -24,7 +30,156 @@ app.config(function($routeProvider){
 });
 
 app.controller('MainController',['$scope', '$http', function($scope, $http){
+	$scope.showSettings = false;
 
+
+	$scope.findPath = function(searchItem){
+		//console.log("Path " + searchItem);
+		$scope.path = {};
+		$http.get("products.json").success(function(data){
+			for(var i = 0; i < data.length; i++)
+			{
+				var category = data[i];
+
+				if(category != undefined)  // sicher nicht nötig
+				{
+					if(category.name != undefined)
+					{
+						$scope.path.categoryName = category.name;
+						if (category.name == searchItem)
+						{
+							$scope.path.depth = 1;                  //Tiefe des Pfads
+							return $scope.path.categoryName;
+						}
+					}
+					if ((category.subCategory != undefined))
+					{
+						for (var j = 0; j < category.subCategory.length; j++)
+						{
+							var subCategory = category.subCategory[j];
+							if (subCategory.name != undefined)
+							{
+								$scope.path.subCategoryName = subCategory.name;
+								if (subCategory.name == searchItem)
+								{
+									$scope.path.depth = 2;
+									return $scope.path.subCategoryName;
+								}
+							}
+							if (subCategory.product != undefined)
+							{
+								for (var k = 0; k < subCategory.product.length; k++)
+								{
+									var product = subCategory.product[k];
+									if (product.name != undefined)
+									{
+										$scope.path.productName = product.name;
+										if (product.name == searchItem) {
+											$scope.path.depth = 3;
+											return $scope.path.productName;
+										}
+									}
+								}//End for()
+							}
+						}//End for()
+					}
+				}
+			}//End for()
+			//console.log($scope.categories);
+		})
+			.error(function(){
+				console.log("Failed to load categories");
+			});
+	};
+
+	this.search = function(){
+
+		var maxSearchResults = 10;
+		$scope.noProductFound = false;
+
+		$scope.products = [];
+		var searchItem = document.getElementsByClassName("searchField")[0].value;
+
+		searchItem = searchItem.toLowerCase();
+		searchItem = searchItem.replace(/ /g, "");
+
+
+		$http.get("products.json").success(function(data){
+			for(var i = 0; i < data.length; i++)
+			{
+				var category = data[i];
+
+				if(category != undefined)  // sicher nicht nötig
+				{
+					/*if(category.name != undefined)
+					{
+						if (category.name.toLowerCase().replace(/ /g, "") == searchItem)
+						{
+							//alle KetegorieDaten finden
+						}
+					}*/
+					if ((category.subCategory != undefined))
+					{
+						for (var j = 0; j < category.subCategory.length; j++)
+						{
+							var subCategory = category.subCategory[j];
+							/*if (subCategory.name != undefined)
+							{
+								if (subCategory.name.toLowerCase().replace(/ /g, "") == searchItem)
+								{
+
+								}
+							}*/
+							if (subCategory.product != undefined)
+							{
+								for (var k = 0; k < subCategory.product.length; k++)
+								{
+									var product = subCategory.product[k];
+									if (product.name != undefined)
+									{
+										var newProductName = product.name.toLowerCase().replace(/ /g, "");
+										var newProductNamePosition = newProductName.search(searchItem);
+
+										if ((newProductNamePosition > 0)&&($scope.products.length < maxSearchResults))
+										{
+											var obj = {};
+											obj.productName = product.name;
+											obj.productImage = product.imageUrl;
+											$scope.products.push(obj);
+
+
+										}
+									}
+								}//End for()
+							}
+						}//End for()
+					}
+				}
+			}//End for()
+
+			console.log("Length" + $scope.products.length);
+
+			if($scope.products.length > 0)
+			{
+				console.log("Drinnen");
+				$scope.subCategoryName = "Die besten " + $scope.products.length + " Suchergebnisse";
+			}
+			else
+			{
+				console.log("Drunter");
+				$scope.subCategoryName = "Leider kein passendes Produkt gefunden";
+				$scope.noProductFound = true;
+				$scope.searchText = "Du kannst ein entsprechendes Produkt gerne Anlegen";
+				//Add blinken lassen
+			}
+
+		})
+		.error(function(){
+			console.log("Failed to load categories");
+		});
+	};
+
+	//Categorien schaffen
 	$scope.categories = [];
 
 	$http.get("products.json").success(function(data){
@@ -59,9 +214,10 @@ app.controller('MainController',['$scope', '$http', function($scope, $http){
 	});
 
 
-	//Nur Data laden von der Kategorie die angeklicked wurde
+	//Nur Data laden von der Kategorie die angeklicked wurde                         Wiso nicht $scope?
 	this.findCategoryContent = function(searchItem){
-		console.log("SubCategory: " + searchItem);
+		//console.log("Category: " + searchItem);
+		$scope.findPath(searchItem);
 		$scope.subCategories = [];
 
 		//Suchalgorythmus
@@ -73,6 +229,7 @@ app.controller('MainController',['$scope', '$http', function($scope, $http){
 				{
 					if (category.name == searchItem)
 					{
+						$scope.categoryName = category.name;
 						if (category.subCategory != undefined)
 						{
 							for (var j = 0; j < category.subCategory.length; j++) {
@@ -100,7 +257,7 @@ app.controller('MainController',['$scope', '$http', function($scope, $http){
 					}
 				}
 			}//End for()
-			console.log($scope.subCategories);
+			//console.log($scope.subCategories);
 		})
 		.error(function(){
 			console.log("Failed to load subCategories");
@@ -109,8 +266,10 @@ app.controller('MainController',['$scope', '$http', function($scope, $http){
 
 
 	this.findSubCategoryContent = function(searchItem){
-		console.log("Product: " + searchItem);
+		//console.log("SubCategory: " + searchItem);
+		$scope.findPath(searchItem);
 		$scope.products = [];
+
 
 		//Suchalgorythmus
 		$http.get("products.json").success(function(data){
@@ -127,6 +286,7 @@ app.controller('MainController',['$scope', '$http', function($scope, $http){
 							var subCategory = category.subCategory[j];
 							if (subCategory.name == searchItem)
 							{
+								$scope.subCategoryName = subCategory.name;
 								if (subCategory.product != undefined)
 								{
 									for (var k = 0; k < subCategory.product.length; k++)
@@ -155,177 +315,177 @@ app.controller('MainController',['$scope', '$http', function($scope, $http){
 					}
 				}
 			}//End for()
-			console.log($scope.products);
+			//console.log($scope.products);
 		})
 		.error(function(){
 			console.log("Failed to load products");
 		});
 	};
+
 }]);
 
 app.controller('ProductController',['$scope', '$http', function($scope, $http){
 
+	var searchItem = "Drehpotentiometer";
+	$scope.productInfo = {
+		/*"productName": "Product1",
+		"imageExisting": true,
+		"productImage": "http://upload.wikimedia.org/wikipedia/commons/b/b5/Potentiometer.jpg",
+		"littleImages": [
+			"http://upload.wikimedia.org/wikipedia/commons/b/b5/Potentiometer.jpg",
+			"http://upload.wikimedia.org/wikipedia/commons/b/b5/Potentiometer.jpg",
+			"http://upload.wikimedia.org/wikipedia/commons/b/b5/Potentiometer.jpg"
+		],
+		"videoExisting": true,
+		"productVideo": "link",
+		"descriptionExisting": true,
+		"description":"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam ",
+		"rankingExisting": true,
+		"dealer":[
+			{
+				"dealerLink":"http://www.conrad.at/ce/de/Welcome.html",
+				"dealerLogo":"http://www.conrad.at/images/default/default/conrad.gif"
+			},
+			{
+				"dealerLink":"http://www.alibaba.com",
+				"dealerLogo":"http://www.momentumsignals.de/wp-content/uploads/breaking-IPO-record.jpg"
+			}
+		],
+		"commentExisting": true,//false
+		"comment":[
+			{
+				"author": "Gordon Shumway",
+				"content": "Lorem Ipsum dolor"
+			},
+			{
+				"author": "John Doe",
+				"content": "Lorem Ipsum dolar sit ament, consetetur"
+			}
+		]*/
+	};
 
 	//Datenbank durchlaufen und daten übertragen
-	this.findProductContent = function(searchItem){
-		$scope.productInfo = {
-		 "productName": "Product1",
-		 "imageExisting": true,
-		 "productImage": "http://upload.wikimedia.org/wikipedia/commons/b/b5/Potentiometer.jpg",
-		 "littleImages": [
-		    "http://upload.wikimedia.org/wikipedia/commons/b/b5/Potentiometer.jpg",
-			"http://upload.wikimedia.org/wikipedia/commons/b/b5/Potentiometer.jpg",
-		    "http://upload.wikimedia.org/wikipedia/commons/b/b5/Potentiometer.jpg"
-		 ],
-		 "videoExisting": false,
-		 "productVideo": "link",
-		 "descriptionExisting": true,
-		 "description":"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam ",
-		 "rankingExisting": true,
-		 "dealer":[
-		    {
-		        "dealerLink":"http://www.conrad.at/ce/de/Welcome.html",
-		        "dealerLogo":"http://www.conrad.at/images/default/default/conrad.gif"
-		    },
-		    {
-		        "dealerLink":"http://www.alibaba.com",
-		        "dealerLogo":"http://www.momentumsignals.de/wp-content/uploads/breaking-IPO-record.jpg"
-		    }
-		 ],
-		 "commentExisting": true,//false
-		 "comment":[
-		    {
-		        "author": "Gordon Shumway",
-		        "content": "Lorem Ipsum dolor"
-		    },
-		    {
-		        "author": "John Doe",
-		        "content": "Lorem Ipsum dolar sit ament, consetetur"
-		    }
-		 ]
-		 };
-		console.log($scope.productInfo);
+	$scope.findProductContent = function(a){
 
-		console.log("ProductContent: " + searchItem);
-		var imageExisting = false;
-		var videoExisting = false;
-		var descriptionExisting = false;
-		var rankingExisting = false;
-		var commentExisting = false;
+		searchItem = a;
+		$scope.showSettings = true;
+		$scope.findPath(searchItem);
+		console.log(searchItem);
 
+	};
 
-		//Suchalgorythmus
-		/*$http.get("products.json").success(function(data){
-			//console.log(data);
-			for(var i = 0; i < data.length; i++)
+	//console.log("searchitem: " + searchItem);
+
+	$http.get("products.json").success(function(data){
+		//console.log(data);
+		for(var i = 0; i < data.length; i++)
+		{
+			var category = data[i];
+			if(category != undefined)
 			{
-				var category = data[i];
-				if(category != undefined)
+				if (category.subCategory != undefined)
 				{
-					if (category.subCategory != undefined)
+					for (var j = 0; j < category.subCategory.length; j++)
 					{
-						for (var j = 0; j < category.subCategory.length; j++)
+						var subCategory = category.subCategory[j];
+						if (subCategory.product != undefined)
 						{
-							var subCategory = category.subCategory[j];
-							if (subCategory.product != undefined)
+							for (var k = 0; k < subCategory.product.length; k++)
 							{
-								for (var k = 0; k < subCategory.product.length; k++)
+								var product = subCategory.product[k];
+								if (product.name == searchItem)
 								{
-									var product = subCategory.product[k];
-									if (product.name == searchItem)
-									{
-										var productName = product.name;
+									var productName = product.name;
 
-										if (product.imageUrl != undefined) {
-											var productImage = product.imageUrl;
-											imageExisting = true;
-										}
-										if (product.littleImages != undefined) {
-											var littleImages = [];
-											for(var l = 0; l < product.littleImages.length; l++)
-											{
-												var littleImage = product.littleImages[l];
-												littleImages.push(littleImage);
-											}
-											imageExisting = true;
-										}
-										if (product.videoUrl != undefined) {
-											var productVideo = product.videoUrl;
-											videoExisting = true;
-										}
-										if (product.description != undefined) {
-											var description = product.description;
-											descriptionExisting = true;
-										}
-										if (product.dealer != undefined) {
-											var dealers = [];
-											for(l = 0; l < product.dealer.length; l++)
-											{
-												var dealerLink = product.dealer[l].dealerLink;
-												var dealerLogo;
-												switch (product.dealer[l].dealerName)
-												{
-													case "Conrad": dealerLogo = "http://www.conrad.at/ce/de/Welcome.html";
-														break;
-													case "Alibaba": dealerLogo = "http://www.alibaba.com";
-														break;
-													case "Amazoon": dealerLogo = "http://www.phpdiver.de/wp-content/uploads/2013/04/amazon.jpg";
-														break;
-													case "Reichelt": dealerLogo = "http://cdn-reichelt.de/bilder/elements/title/Reichelt-Logo_pdf.gif";
-														break;
-													default : dealerLogo = "";
-														break;
-												}
-												var dealer = new Object();
-												dealer.dealerLinks = dealerLink;
-												dealer.dealerLogos = dealerLogo;
-												dealers.push(dealer);
-											}
-											rankingExisting = true;
-										}
-										if (product.comment != undefined) {
-											var comments = [];
-											for(l = 0; l < product.comment.length; l++)
-											{
-												var author = product.comment[l].author;
-												var content = product.comment[l].content;
-
-												var comment = new Object();
-												comment.authors = author;
-												comment.contents = content;
-												comments.push(comment);
-											}
-											commentExisting = true;
-										}
-
-										$scope.productInfo.productName = productName;
-										$scope.productInfo.productImage = productImage;
-										$scope.productInfo.littleImages = littleImages;
-										$scope.productInfo.productVideo = productVideo;
-										$scope.productInfo.description = description;
-										$scope.productInfo.dealers = dealers;
-										$scope.productInfo.comments = comments;
-										$scope.productInfo.imageExisting = imageExisting;
-										$scope.productInfo.videoExisting = videoExisting;
-										$scope.productInfo.descriptionExisting = descriptionExisting;
-										$scope.productInfo.rankingExisting = rankingExisting;
-										$scope.productInfo.commentExisting = commentExisting;
-
-
+									if (product.imageUrl != undefined) {
+										var productImage = product.imageUrl;
+										var imageExisting = true;
 									}
+									if (product.littleImages != undefined) {
+										var littleImages = [];
+										for(var l = 0; l < product.littleImages.length; l++)
+										{
+											var littleImage = product.littleImages[l];
+											littleImages.push(littleImage);
+										}
+										imageExisting = true;
+									}
+									if (product.videoUrl != undefined) {
+										var productVideo = product.videoUrl;
+										var videoExisting = true;
+									}
+									if (product.description != undefined) {
+										var description = product.description;
+										var descriptionExisting = true;
+									}
+									if (product.dealer != undefined) {
+										var dealers = [];
+										for(l = 0; l < product.dealer.length; l++)
+										{
+											var dealerLink = product.dealer[l].dealerLink;
+											var dealerLogo;
+											switch (product.dealer[l].dealerName)
+											{
+												case "Conrad": dealerLogo = "http://www.conrad.at/ce/de/Welcome.html";
+													break;
+												case "Alibaba": dealerLogo = "http://www.alibaba.com";
+													break;
+												case "Amazoon": dealerLogo = "http://www.phpdiver.de/wp-content/uploads/2013/04/amazon.jpg";
+													break;
+												case "Reichelt": dealerLogo = "http://cdn-reichelt.de/bilder/elements/title/Reichelt-Logo_pdf.gif";
+													break;
+												default : dealerLogo = "";
+													break;
+											}
+											var dealer = new Object();
+											dealer.dealerLinks = dealerLink;
+											dealer.dealerLogos = dealerLogo;
+											dealers.push(dealer);
+										}
+										var rankingExisting = true;
+									}
+									if (product.comment != undefined) {
+										var comments = [];
+										for(l = 0; l < product.comment.length; l++)
+										{
+											var author = product.comment[l].author;
+											var content = product.comment[l].content;
+
+											var comment = new Object();
+											comment.authors = author;
+											comment.contents = content;
+											comments.push(comment);
+										}
+										var commentExisting = true;
+									}
+
+									$scope.productInfo.productName = productName;
+									$scope.productInfo.productImage = productImage;
+									$scope.productInfo.littleImages = littleImages;
+									$scope.productInfo.productVideo = productVideo;
+									$scope.productInfo.description = description;
+									$scope.productInfo.dealers = dealers;
+									$scope.productInfo.comments = comments;
+									$scope.productInfo.imageExisting = imageExisting;
+									$scope.productInfo.videoExisting = videoExisting;
+									$scope.productInfo.descriptionExisting = descriptionExisting;
+									$scope.productInfo.rankingExisting = rankingExisting;
+									$scope.productInfo.commentExisting = commentExisting;
+
+
 								}
 							}
 						}
 					}
 				}
-			}//End for()
-			console.log($scope.productInfo);
-		})
+			}
+		}//End for()
+
+	})
 		.error(function(){
 			console.log("Failed to load products");
-		});*/
-
-	}
+		});
+	//console.log($scope.productInfo);
 }]);
 
 app.controller('NavigationController',['$http', "$scope", function($http, $scope){
--- js/styles.js
+++ js/styles.js
@@ -1,13 +1,3 @@
 /**
  * Created by User on 12.10.2014.
  */
-var styles = function(){
-	var navBarHeight = document.getElementsByClassName("navigationBar")[0].style.height;
-	var mainHeight = document.getElementsByClassName("main")[0].style.height;
-
-	console.log(navBarHeight+" : "+ mainHeight);
-	navBarHeight = mainHeight;
-
-
-
-};
